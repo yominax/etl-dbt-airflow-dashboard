25aad290bbfba907603ee4b91386dd6e1a0e8e86
Sidi Mohamed MOUTAOUAFFIQ <sidimoh.mouta@gmail.com>
2025-10-20 14:55:20 +0200

    providers

--- include/terraform/providers.tf
+++ include/terraform/providers.tf
@@ -1,1 +1,14 @@
- 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  
+  features {}
+}
