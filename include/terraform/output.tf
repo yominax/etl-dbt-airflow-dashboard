0af7bc1192adc7c8a866d9f44ed818b85d93d1f4
Sidi Mohamed MOUTAOUAFFIQ <sidimoh.mouta@gmail.com>
2025-10-20 16:49:18 +0200

    Output.tf correction variable

--- include/terraform/output.tf
+++ include/terraform/output.tf
@@ -1,24 +1,20 @@
 output "adls_access_key" {
-  value       = azurerm_storage_account.adls.primary_access_key
-  sensitive   = true
+  value     = azurerm_storage_account.lakehouse.primary_access_key
+  sensitive = true
 }
 
 output "bronze_container_name" {
-  value = azurerm_storage_container.containers["bronze"].name
+  value = azurerm_storage_container.layers["bronze"].name
 }
 
 output "silver_container_name" {
-  value = azurerm_storage_container.containers["silver"].name
+  value = azurerm_storage_container.layers["silver"].name
 }
 
 output "gold_container_name" {
-  value = azurerm_storage_container.containers["gold"].name
+  value = azurerm_storage_container.layers["gold"].name
 }
 
 # output "key_vault_id" {
-#   value = azurerm_key_vault.kv.id
-# }
-
-# output "adf_managed_identity_object_id" {
-#   value       = azurerm_data_factory.adf.identity[0].principal_id
+#   value = azurerm_key_vault.vault.id
 # }
